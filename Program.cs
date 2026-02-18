14e5f1fa662076bbfef59c459b2e58c34220c54f
Katherine Moore <km2562@bath.ac.uk>
2026-02-18 08:57:45 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,73 +1,118 @@
+using HARAnalyser;
 using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using System.Reflection;
 using System.Text;
-using System.Text.Json.Serialization;
-using static ConsoleApp1.PaymentHar;
+
+Console.WriteLine("HAR Analyser");
+
+// This is a bespoke app written to convert the HAR file from JSON and to extract key data items to csv
+// It has a second purpose to search for strings within the webpages. 
+// This app relies on the HAR and webfiles being added to a folder on the C drive named "HAR"
+// The HAR file is hardcoded as "www.tkmaxx.com.har"
+// The webpage is hardcoded as "Payment powered by Worldpay - TK Maxx UK_files"
 
 HelperFunctions help = new HelperFunctions();
 
-string path = @".\outputHAR3.csv";
-
-if (!File.Exists(path))
-{
-    // Create a file to write to.
-    string[] createText = { "" };
-    File.WriteAllLines(path, createText, Encoding.UTF8);
-}
-
-string readText = File.ReadAllText(@".\www.tkmaxx.com.har");
-
-PaymentHar? fullHAR = JsonConvert.DeserializeObject<PaymentHar>(readText);
-
+// Read the HAR file and deserialise 
+string readText = File.ReadAllText(@"C:\\HAR\www.tkmaxx.com.har");
+PaymentHar fullHAR = JsonConvert.DeserializeObject<PaymentHar>(readText);
 var entries = fullHAR.log.entries;
 
-List<OutputFile> newFile = new List<OutputFile>();
+bool repeat = true;
 
-foreach (var entry in entries)
+while (repeat)
 {
+    Console.WriteLine("Choose a function");
+    Console.WriteLine("[1] Extract URL details from HAR to file");
+    Console.WriteLine("[2] Search in files");
+    Console.WriteLine("[3] Exit");
 
+    var input = Console.ReadLine();
 
-    var outputFile = new OutputFile();
-
-    outputFile.Timestamp = entry.startedDateTime;
-    outputFile.RequestUrl = entry.request.url;
-    outputFile.Method = entry.request.method;
-    outputFile.ResponseStatus = entry.response.status;
-    outputFile.ResponseStatusText = entry.response.statusText;
-
-    newFile.Add(outputFile);
-
-}
-
-
-
-using (var writer = new StringWriter())
-{
-    var csvBuilder = new StringBuilder();
-
-    // Write header
-    PropertyInfo[] properties = typeof(OutputFile).GetProperties();
-    string header = string.Join(",", properties.Select(p => p.Name));
-    csvBuilder.AppendLine(header);
-
-    // Write data
-    foreach (var data in newFile)
+    if (input == "1")
     {
-        string csvLine = string.Join(",", properties.Select(p => help.EscapeCsvField(p.GetValue(data)?.ToString())));
-        csvBuilder.AppendLine(csvLine);
+
+        string path = @"C:\\HAR\outputHAR.csv";
+
+        if (!File.Exists(path))
+        {
+            // Create a file to write to.
+            string[] createText = { "" };
+            File.WriteAllLines(path, createText, Encoding.UTF8);
+        }
+
+        List<OutputFile> newFile = new List<OutputFile>();
+
+        foreach (var entry in entries)
+        {
+            var outputFile = new OutputFile();
+
+            outputFile.Timestamp = entry.startedDateTime;
+            outputFile.RequestUrl = entry.request.url;
+            outputFile.Method = entry.request.method;
+            outputFile.ResponseStatus = entry.response.status;
+            outputFile.ResponseStatusText = entry.response.statusText;
+
+            newFile.Add(outputFile);
+        }
+
+        using (var writer = new StringWriter())
+        {
+            var csvBuilder = new StringBuilder();
+
+            // Write header
+            PropertyInfo[] properties = typeof(OutputFile).GetProperties();
+            string header = string.Join(",", properties.Select(p => p.Name));
+            csvBuilder.AppendLine(header);
+
+            // Write data
+            foreach (var data in newFile)
+            {
+                string csvLine = string.Join(",", properties.Select(p => help.EscapeCsvField(p.GetValue(data)?.ToString())));
+                csvBuilder.AppendLine(csvLine);
+            }
+
+            string asString = csvBuilder.ToString();
+
+            File.AppendAllText(path, asString);
+        }
+
     }
 
-    string asString = csvBuilder.ToString();
+    else if (input == "2")
+    {
 
-    File.AppendAllText(path, asString);
+        Console.Write("Search term: ");
+        string searchTerm = Console.ReadLine();
+        int count = 0;
 
+        string[] fileList = Directory.GetFiles(@"C:\\HAR\Payment powered by Worldpay - TK Maxx UK_files");
 
+        foreach (var file in fileList)
+        {
+            string contents = File.ReadAllText(file);
+
+            if (contents.Contains(searchTerm, StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.WriteLine(file);
+                Console.WriteLine(" ");
+                count++;
+            }
+        }
+
+        if (count == 0)
+        {
+            Console.WriteLine("No instances found");
+            Console.WriteLine(" ");
+        }
+    }
+
+    else if (input == "3")
+    {
+        repeat = false;
+    }
+    else
+    {
+        Console.WriteLine("Input not recognised, try again");
+    }
 }
-
-Console.WriteLine("breakpoint");
-
-
